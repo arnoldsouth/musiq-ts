2e9a4b3082ac5bb6caaca788c2f1549b49dc4561
arnoldsouth <arnoldsouth@gmail.com>
2024-03-05 06:30:14 -0600

    add constants to spotify api

--- src/utils/SpotifyApi.ts
+++ src/utils/SpotifyApi.ts
@@ -1,5 +1,22 @@
+interface SpotifyApi {
+  generateRandomString(length: number): string;
+}
+
 const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 const redirectUri = process.env.REACT_APP_SPOTIFY_REDIRECT_URI;
+
+const authorizationEndpoint = process.env.REACT_APP_SPOTIFY_AUTH_ENDPOINT;
+const tokenEndpoint = process.env.REACT_APP_SPOTIFY_TOKEN_ENDPOINT;
+const scope = process.env.REACT_APP_SPOTIFY_SCOPE;
+
+const generateRandomString = (length: number) => {
+  const possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const values = crypto.getRandomValues(new Uint8Array(length));
+  return values.reduce((acc, x) => acc + possible[x % possible.length], '');
+};
+
+const codeVerifier = generateRandomString(64);
 
 const SpotifyApi = {
   getAccessToken() {
